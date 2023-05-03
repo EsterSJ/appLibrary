708bdc845adc2dd7690f4d483d42d636c39e8ca0
EsterSJ <ester_s@hotmail.com>
2023-05-03 12:13:03 +0200

    retos dia2 con opcional 1 terminados

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,40 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+  // @ViewChild('modificado') cambios: ElementRef;
+  // @ViewChild('no_mofificado') no_cambios: ElementRef;
+
+  public perfil: User;
+
+  constructor(){
+    this.perfil = new User(1234,"Ester","Sánchez Jiménez","ester_s@hotmail.com","assets/img/avatar.png","1234");
+  }
+
+  public modificar_datos(name: string, last_name: string, email: string, photo: string){  
+      
+    let datos_modificados: boolean = false;
+
+      if (name != ''){
+        this.perfil.name = name;
+        datos_modificados = true;
+      }
+      if (last_name != ''){
+        this.perfil.last_name = last_name;
+        datos_modificados = true;
+      }
+      if (email != ''){
+        this.perfil.email = email;
+        datos_modificados = true;
+      }
+      if (photo != ''){
+        this.perfil.photo = photo;
+        datos_modificados = true;
+      }
+  }
+}
